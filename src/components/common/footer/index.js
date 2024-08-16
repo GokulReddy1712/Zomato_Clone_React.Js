f8cc2f6df23a64c6822b2477b69aae88dddc8453
GokulReddy1712 <gokul17122001@gmail.com>
2024-08-16 11:29:11 +0530

    Initial commit

---
+++ src/components/common/footer/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../footer/footer.css";
+
+const Footer = () => {
+  return <div className="absolute-center">Made with ❤️ by Gokul</div>;
+};
+
+export default Footer;
